2c4ea31bb40fe30db9138c47468472a86921eea2
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-01 23:34:00 -0500

    feat: invocacion a HTTP get JS

--- src/breakingbad/breakingbad-app.js
+++ src/breakingbad/breakingbad-app.js
@@ -1,9 +1,31 @@
 /**
+ * @returns {Object} Quote Information
+ */
+const fetchQuote = async () => {
+    const response = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+    const data = await response.json();
+    return (!data) ? null : data[0];
+}
+/**
  * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
     const appTitle = document.getElementById('h1AppTitle');
     appTitle.innerHTML = 'BreakingBad App';
     element.innerHTML = 'Loading...';
+
+    const quoteLabel = document.createElement('blockquote');
+    const authorLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+    const renderQuote = (data) => {
+        if(!data) return;
+        quoteLabel.innerHTML = data.quote;
+        authorLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authorLabel, nextQuoteButton);
+    }
+
+    fetchQuote().then(renderQuote);
 }
