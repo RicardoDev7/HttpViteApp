fb8dfa0465e19e50085fb331459e24a9dd58e014
Ricardo Martin Metzger Mejia <ricardo7.dev@gmail.com>
2025-12-03 00:17:53 -0500

    feat: mapper de datos en JS

---
+++ src/users/mappers/localhost-user.mapper.js
@@ -0,0 +1,27 @@
+import { User } from "../models/user"
+
+/**
+ * 
+ * @param {Like<User>} localHostUser 
+ * @returns {User}
+ */
+export const localHostUserToModel = (localHostUser) => {
+    const {
+        id,
+        isActive,
+        balance,
+        avatar,
+        first_name,
+        last_name,
+        gender
+    } = localHostUser;
+    return new User({
+        id, 
+        isActive,
+        balance, 
+        avatar, 
+        firstName: first_name, 
+        lastName: last_name, 
+        gender
+    });
+}
